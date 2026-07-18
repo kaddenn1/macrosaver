e781729e09d6a875120f257197c47bf65c712843
kaddenn1 <al.datadoctors@gmail.com>
2026-07-18 10:27:02 -0700

    Remove /cassie mockup page
    It was getting picked up by Google Search Console despite noindex.

--- components/SiteChrome.tsx
+++ components/SiteChrome.tsx
@@ -1,18 +1,11 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import AmbientBackground from "./AmbientBackground";
 import CompareTray from "./CompareTray";
 
 export default function SiteChrome({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-
-  if (pathname?.startsWith("/cassie")) {
-    return <>{children}</>;
-  }
-
   return (
     <>
       <AmbientBackground />
